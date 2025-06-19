455b3544982f18e4228c6c88589e1e57f4735184
Sheharyar <Sheharyarmalik2019@gmail.com>
2025-06-20 01:02:56 +0500

    db connection

---
+++ src/db/index.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose"
+import {DB_NAME} from "../constents/index.js"
+const datebsae_connection = async()=>{
+    try{
+     let connected = await mongoose.connect(`${process.env.DATABASE_SRING}/${DB_NAME}`)
+     console.log("dataBase connected Successfully", `HOST IS ${connected.connection.host}`)
+    }catch(error){
+     console.log(`Error While connecting DataBAse${error.message}`);
+    }
+}
+export  {datebsae_connection}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
+import 'dotenv/config'
+import {datebsae_connection} from "./db/index.js"
+
+datebsae_connection()
+
 console.log("new start");
