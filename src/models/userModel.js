987366a2ab7b8eb4d1581f98a8e7ff4a4ad6a859
Sheharyar <Sheharyarmalik2019@gmail.com>
2025-06-22 21:37:13 +0500

    pre hook and multer + cloudinary configration

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,5 +1,6 @@
 import mongoose,{Schema} from "mongoose";
-
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"  
 const userSchema = new Schema({
     userName:{
         type:String,
@@ -42,5 +43,43 @@ const userSchema = new Schema({
 
     }
 },{timestamps:true})
+
+
+userSchema.pre("save",async function(next){
+    if(!this.isModified("password")) return next()
+    this.password = bcrypt.hash(this.password,10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function(password){
+   return await bcrypt.compare(password,this.password)
+} 
+
+userSchema.methods.genrateAccessToken = function(){
+    return jwt.sign(
+        {
+        _id:this._id,
+        userName:this.userName,
+        fullName:this.fullName,
+        email:this.email
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+    }
+)
+}
+userSchema.methods.genrateRefreshToken = function(){
+    return jwt.sign(
+        {
+        _id:this._id,
+      
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+    }
+)
+}
 
 export const User = mongoose.model("User",userSchema)
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from 'fs';
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+}); 
+
+const uploadfile = async(filepath)=>{
+    try{
+        if(!filepath)  throw new Error("File path is required");
+        if(!fs.existsSync(filepath)) {
+            throw new Error("File does not exist at the specified path");
+        }
+        const response = await cloudinary.uploader.upload("filepath",{
+            resource_type: "auto",
+            folder: "youtube-app"
+        })
+        console.log("File uploaded successfully:", response.url);
+        return response
+    }catch(error){
+        fs.unlinkSync(filepath); // Delete the file from local storage
+        console.error("Error uploading file:", error);
+        return error
+    }
+} 
+
+export {uploadfile};
+
